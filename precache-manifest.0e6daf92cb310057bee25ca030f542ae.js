c15a1a888ed16f63a0922a77c5f7ba35558edaea
Abdoul Tare <abdoultare@Abdouls-MacBook-Pro.local>
2020-02-22 00:27:33 -0500

    Updates

---
+++ precache-manifest.0e6daf92cb310057bee25ca030f542ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfc0fd7fa77dcb9ec4275535a3f40f2",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "8721899b17cdcdf63967",
+    "url": "/monsters-rodolex/static/css/main.c564263e.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rodolex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rodolex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8721899b17cdcdf63967",
+    "url": "/monsters-rodolex/static/js/main.8b2fcdb9.chunk.js"
+  },
+  {
+    "revision": "c185d5c9465dcb4ee29e",
+    "url": "/monsters-rodolex/static/js/runtime-main.0e72fa14.js"
+  }
+]);
